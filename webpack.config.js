2785c357015e349e7535e33ab36d94273712e5ee
soobing <qls0147@naver.com>
2020-04-11 21:47:44 +0900

    react, webpack, babel 설정 완료

---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+const path = require("path");
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const mode = process.env.NODE_ENV || "development";
+
+module.exports = {
+  mode: mode,
+  entry: {
+    main: './src/index.js'
+  },
+  output: {
+    filename: '[name].js',
+    path: path.resolve("./dist"),
+    publicPath: '/'
+  },
+  devServer: {
+    historyApiFallback: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
+        }
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'public/index.html'
+    })
+  ]
+};
