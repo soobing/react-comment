b7bf41391ed3d7bf64d6bd5ce1b0d04c314bc92a
soobing <qls0147@naver.com>
2020-04-12 15:33:10 +0900

    reducer에 저장되있는 데이터 보여주도록 수정

--- src/pages/comments.js
+++ src/pages/comments.js
@@ -10,13 +10,17 @@ export default function commentsPage() {
   return <div>
     <h1 onClick={() => dispatch({ type: types.APP_ACTION_TEST, data: !comments.isTest })}>Comments Page</h1>
     <p>{String(comments.isTest)}</p>
-    <Comment value={data[0].value} onChange={(e) =>
-      dispatch({
-        type: types.COMMENT_SET_VALUE, data: {
-          id: 0,
-          key: 'value',
-          value: e.target.value
-        }
-      })} />
+    {
+      data.map((item) => {
+        return <Comment value={item.value} onChange={(e) =>
+          dispatch({
+            type: types.COMMENT_SET_VALUE, data: {
+              id: item.id,
+              key: 'value',
+              value: e.target.value
+            }
+          })} />
+      })
+    }
   </div>
 }
