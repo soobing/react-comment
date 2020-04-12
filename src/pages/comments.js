e56eda9697a2f7f5253d8dd656f1073c38f436ec
soobing <qls0147@naver.com>
2020-04-12 14:29:48 +0900

    오타 수정

--- src/pages/comments.js
+++ src/pages/comments.js
@@ -2,7 +2,7 @@ import React from 'react';
 import Comment from '../components/comment';
 export default function commentsPage() {
   return <div>
-    <h1>Comment Page</h1>
+    <h1>Comments Page</h1>
     <Comment />
     <Comment />
   </div>
