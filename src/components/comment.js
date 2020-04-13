9f6b7939ef96440ddd31fe814720275bb4a56f71
soobing <qls0147@naver.com>
2020-04-13 21:57:20 +0900

    scroll처리

--- src/components/comment.js
+++ src/components/comment.js
@@ -58,7 +58,6 @@ export default function comment({ item, reply, onChangeValue, onClickDelete }) {
                   }
                 })
                 e.target.value = '';
-                e.target.scrollIntoView();
                 e.preventDefault();
                 e.stopPropagation();
               }
