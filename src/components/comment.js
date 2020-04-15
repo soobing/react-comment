7a2e0ae2f6ba1ab6cbd68758d7c8a4d7ad46089c
soobing <qls0147@naver.com>
2020-04-15 11:59:09 +0900

    위치 변경

--- src/components/comment.js
+++ src/components/comment.js
@@ -7,6 +7,7 @@ import '../assets/comment.css'
 import more from '../assets/images/more.png'
 
 export default function orange({ replyCallback, item }) {
+  const [showMore, setShowMore] = useState(false);
   const dispatch = useDispatch();
   return <div className='comment'>
     <TextArea
@@ -25,18 +26,22 @@ export default function orange({ replyCallback, item }) {
       <div>좋아요</div>
       {replyCallback && <div onClick={replyCallback}>답글달기</div>}
       <div className='more-wrapper'>
-        <img src={more} />
-        <div className='more-modal'>
-          <div>수정하기...</div>
-          <div onClick={(e) => {
-            dispatch({
-              type: types.COMMENT_DELETE,
-              data: {
-                id: item.id
-              }
-            })
-          }}>삭제하기...</div>
-        </div>
+        <img src={more} alt='more' onClick={() => { setShowMore(true) }} />
+        {
+          showMore ?
+            <div className='more-modal'>
+              <div>수정하기...</div>
+              <div onClick={(e) => {
+                dispatch({
+                  type: types.COMMENT_DELETE,
+                  data: {
+                    id: item.id
+                  }
+                })
+              }}>삭제하기...</div>
+            </div>
+            : null
+        }
       </div>
     </div>
   </div>
