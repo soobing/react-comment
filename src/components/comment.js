ed96fb8de5b17d00e0a57ae6e1df3776356c4bc1
soobing <qls0147@naver.com>
2020-04-15 12:39:20 +0900

    모달 추가

--- src/components/comment.js
+++ src/components/comment.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import * as types from '../actions';
 
@@ -6,9 +6,20 @@ import TextArea from '../components/textarea';
 import '../assets/comment.css'
 import more from '../assets/images/more.png'
 
-export default function orange({ replyCallback, item }) {
+export default function comment({ replyCallback, item }) {
   const [showMore, setShowMore] = useState(false);
   const dispatch = useDispatch();
+  const onWindowClick = (event) => {
+    if (!event.target.classList.contains('more')) {
+      setShowMore(false)
+    }
+  }
+  useEffect(() => {
+    window.addEventListener('click', onWindowClick, false);
+    return () => {
+      window.removeEventListener('click', onWindowClick, false);
+    }
+  }, [])
   return <div className='comment'>
     <TextArea
       item={item}
@@ -26,7 +37,7 @@ export default function orange({ replyCallback, item }) {
       <div>좋아요</div>
       {replyCallback && <div onClick={replyCallback}>답글달기</div>}
       <div className='more-wrapper'>
-        <img src={more} alt='more' onClick={() => { setShowMore(true) }} />
+        <img src={more} alt='more' className='more' onClick={(e) => setShowMore(!showMore)} />
         {
           showMore ?
             <div className='more-modal'>
--- src/pages/comments.js
+++ src/pages/comments.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import Comment from '../components/comment';
 import TextArea from '../components/textarea';
 import { useDispatch, useSelector } from 'react-redux';
